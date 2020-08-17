3b3e11d833d83369811612068733c33e4dad3f1f
bchwtz <benjamin.buchwitz@gmail.com>
2020-08-17 16:32:37 +0200

    Initial Commit

--- script/setup_hadoop.sh
+++ script/setup_hadoop.sh
@@ -5,6 +5,6 @@ chown -R jovyan:users /opt/hadoop-3.3.0
 
 
 /etc/init.d/ssh start
-hdfs namenode -format
-start-dfs.sh
+/opt/hadoop-3.3.0/bin/hdfs namenode -format
+/opt/hadoop-3.3.0/bin/start-dfs.sh
 #start-yarn.sh
