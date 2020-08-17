dd2954689d55124ffc050c1ba6b0ffa9619adef9
bchwtz <benjamin.buchwitz@gmail.com>
2020-08-17 16:26:32 +0200

    Initial Commit

--- script/setup_hadoop.sh
+++ script/setup_hadoop.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-wget ftp://ftp.fau.de/apache/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
-tar -xzf hadoop-3.3.0.tar.gz
-mv hadoop-3.3.0 /opt/hadoop-3.3.0
-rm hadoop-3.3.0.tar.gz
+
+chown -R jovyan:users /opt/hadoop-3.3.0
+
+
+/etc/init.d/ssh start
+hdfs namenode -format
+start-dfs.sh
+#start-yarn.sh
--- script/setup_userprofile.sh
+++ script/setup_userprofile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+
 echo -e "\nexport PDSH_RCMD_TYPE=ssh" >> ~/.bashrc
 
 echo -e "\n" >> ~/.bashrc 
@@ -10,3 +14,5 @@ echo 'export HADOOP_MAPRED_HOME=${HADOOP_HOME}' >> ~/.bashrc
 echo 'export HADOOP_COMMON_HOME=${HADOOP_HOME}' >> ~/.bashrc
 echo 'export HADOOP_HDFS_HOME=${HADOOP_HOME}' >> ~/.bashrc
 echo 'export YARN_HOME=${HADOOP_HOME}' >> ~/.bashrc
+
+echo 'export JAVA_HOME=$(dirname $(dirname $(update-alternatives --list java)))' >> /opt/hadoop-3.3.0/etc/hadoop/hadoop-env.sh
